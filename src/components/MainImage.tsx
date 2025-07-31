19e9919356296610acb96e3dfb52eb8f69f5c5a0
Vasyl Savka <grumpy.ancient@gmail.com>
2025-07-31 16:11:00 +0300

    fix bug with mainImage pop-up

--- src/components/MainImage.tsx
+++ src/components/MainImage.tsx
@@ -2,7 +2,7 @@ import type { MouseEventHandler } from 'react';
 const MainImage = ({ curImage, onClick }: { curImage: string; onClick?: MouseEventHandler }) => (
   <img
     onClick={onClick}
-    className="col-span-4 row-span-2 lg:rounded-2xl"
+    className="pointer-events-none col-span-4 row-span-2 lg:pointer-events-auto lg:rounded-2xl"
     src={curImage}
     alt="sneakers front big"
   />
